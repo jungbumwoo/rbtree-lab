002f3121b051b4976f18d7e799c615db7a182271
jungbumwoo <jungbumwoo@naver.com>
2021-09-06 15:15:19 +0900

    Err: segmentation falut / insert & rotate

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,6 +1,9 @@
 #include "rbtree.h"
-
 #include <malloc.h>
+#include <stdbool.h> // import false
+
+/* functions */
+node_t *rotate(const rbtree *t, const key_t key, node_t *pivot);
 
 rbtree *new_rbtree(void)
 {
@@ -26,6 +29,40 @@ node_t *rbtree_insert(const rbtree *t, const key_t key)
   ggp = gp = p = (node_t *)t->root;
   curr = (node_t *)t->root->left;
 
+  while (curr)
+  {
+    if (key == curr->key)
+      return false; // return false 말고 다른거 리턴해줘야하나? 중복방지
+
+    // 자식 노드 둘 다 있고 빨강이면 color promotion
+    if (curr->left && curr->right && curr->left->color == RBTREE_RED && curr->right->color == RBTREE_RED)
+    {
+      curr->color = RBTREE_RED;
+      curr->left->color = curr->right->color = RBTREE_BLACK;
+
+      // 부모가 빨강이면 rotation
+      if (p->color == RBTREE_RED)
+      {
+        gp->color = RBTREE_RED;
+        if ((key > gp->key) != (key > p->key))
+          p = rotate(t, key, gp);
+        curr = rotate(t, key, ggp);
+        curr->color = RBTREE_RED;
+      }
+
+      // 뿌리는 항상 검정
+      t->root->left->color = RBTREE_BLACK; // 여기 화살표 왜 빨강색이야 무섭게 ㄷㄷ
+    }
+    // 한 칸씩 내려가기
+    ggp = gp;
+    gp = p;
+    p = curr;
+    if (key > curr->key)
+      curr = curr->right;
+    else
+      curr = curr->left;
+  }
+
   // while
 
   node_t *temp = (node_t *)malloc(sizeof(node_t));
@@ -38,7 +75,20 @@ node_t *rbtree_insert(const rbtree *t, const key_t key)
   else
     p->left = temp;
 
-  return temp;
+  // 부모가 빨강이면 회전!
+  if (p->color == RBTREE_RED)
+  {
+    gp->color = RBTREE_RED;
+    if ((key > gp->key) != (key > p->key))
+      p = rotate(t, key, gp);
+    curr = rotate(t, key, ggp);
+    curr->color = RBTREE_RED;
+  }
+
+  // root는 항상 검정으로
+  t->root->left->color = RBTREE_RED;
+
+  return curr;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key)
@@ -69,4 +119,33 @@ int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
 {
   // TODO: implement to_array
   return 0;
+}
+
+/* functions */
+node_t *rotate(const rbtree *t, const key_t key, node_t *pivot)
+{
+  node_t *child, *gchild;
+  if ((key > pivot->key || key == pivot->key) && pivot != t->root)
+    child = (node_t *)pivot->right;
+  else
+    child = (node_t *)pivot->left;
+
+  if (key > child->key || key == child->key) //Rotate Left
+  {
+    gchild = (node_t *)child->right;
+    child->right = gchild->left;
+    gchild->left = (node_t *)child;
+  }
+  else
+  {
+    gchild = (node_t *)child->left;
+    child->left = gchild->right;
+    gchild->right = child->left;
+  }
+  if ((key > pivot->key || key == pivot->key) && pivot != t->root)
+    pivot->right = gchild;
+  else
+    pivot->left = gchild;
+
+  return gchild;
 }
